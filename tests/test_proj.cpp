f341b26dd87df8960b003c5a95732490bb079620
alex <iteeshkeen@gmail.com>
2026-08-21 17:25:37 +0500

    test: add test for read file, check length of readed part

--- tests/test_proj.cpp
+++ tests/test_proj.cpp
@@ -1,4 +1,10 @@
 #include "../includes/hexdump.h"
+#include "../includes/reader.h"
 #include <gtest/gtest.h>
 
 TEST(ProjTest, Example) { EXPECT_EQ(example(), 1); };
+
+TEST(ProjTest, ReadFile) {
+  filedata data = readfile("hello.txt");
+  EXPECT_EQ(data.length, 13);
+}
