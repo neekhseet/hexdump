6fc955ab548ae0fbd5303252cbe8eb9e94865601
alex <iteeshkeen@gmail.com>
2026-08-22 19:53:43 +0500

    chore: del mock code

--- tests/test_proj.cpp
+++ tests/test_proj.cpp
@@ -2,8 +2,6 @@
 #include "../includes/reader.h"
 #include <gtest/gtest.h>
 
-TEST(ProjTest, Example) { EXPECT_EQ(example(), 1); };
-
 TEST(ProjTest, ReadFile) {
   filedata data = readfile("hello.txt");
   EXPECT_EQ(data.length, 13);
