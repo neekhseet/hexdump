c55f8f340a058401f2514bd573f138e6b09d0593
neekhseet <iteeshkeen@gmail.com>
2026-09-12 21:08:30 +0500

    test: read file creates linked list (pass)

--- tests/test_proj.cpp
+++ tests/test_proj.cpp
@@ -9,3 +9,16 @@ TEST(ProjTest, ReadFile)
   EXPECT_EQ(memcmp(readed->data, "Hello", 5), 0);
   EXPECT_STREQ(readed->data, "Hello, world!");
 }
+
+TEST(ProjTest, ReadFileCreatesLinkedList)
+{
+  Bytes *readed = readfile("linked.txt");
+
+  ASSERT_NE(readed, nullptr);
+  ASSERT_NE(readed->next, nullptr);
+
+  EXPECT_EQ(memcmp(readed->data, "ABCDEFGHIJKLMNOP", 16), 0);
+  EXPECT_EQ(memcmp(readed->next->data, "abcdefghijklmnop", 16), 0);
+
+  EXPECT_EQ(readed->next->next, nullptr);
+}
